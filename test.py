d02c536b55b5bca23d3b4f6a7f71f58cc19e6b36
Maxime Ellerbach <maxime@ellerbach.net>
2020-12-23 01:16:22 +0100

    cleanup

--- test.py
+++ test.py
@@ -1,180 +1,149 @@
-import autolib
-import interface
-import math
 import os
 import time
 
 import cv2
-import keras.backend as K
-import keyboard
-import mss
+import d3dshot
 import numpy as np
-import pyautogui
 import pyvjoy
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-from keras.losses import mse
-from keras.models import load_model
-from PIL import Image
+import tensorflow
+from custom_modules import architectures
+from custom_modules.datasets import dataset_json
 
-pyautogui.FAILSAFE = False
+import xinput
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True # dynamically grow the memory used on the GPU
-sess = tf.Session(config=config)
-config.log_device_placement = True  # to log device placement (on which device the operation ran)
-set_session(sess) # set this TensorFlow session as the default
+physical_devices = tensorflow.config.list_physical_devices('GPU')
+for gpu_instance in physical_devices:
+    tensorflow.config.experimental.set_memory_growth(gpu_instance, True)
 
-def round_list(iterable, n=3):
-    rounded = []
-    for i in iterable:
-        rounded.append(round(i, n))
-    return rounded
+joysticks = xinput.XInputJoystick.enumerate_devices()
+print('found %d devices' % (len(joysticks)))
 
-class joy_keyboard():
-    """
-    class to use keyboard to encode direction to the game
-    """
-    def __init__(self):
-        self.i = 0
-        self.kdict = ['left', 'right', 'up', 'down']
-        self.k = self.kdict[-1]
-        self.idt = 1/30
-        self.dt = self.idt
-        self.pred = 0
+joy = joysticks[0]
 
-    def iterate(self):
-        pyautogui.keyDown(self.k)
-        time.sleep(0.1)
-        pyautogui.keyUp(self.k)
+controller_values = {
+    'l_thumb_x': 0.0,
+    'l_thumb_y': 0.0,
+    'right_trigger': 0.0,
+    'left_trigger': 0.0,
+    'r_thumb_x': 0.0,
+    'r_thumb_y': 0.0,
+    1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0
+}
 
-    def get_key(self, direct, prev):
-        if prev>0:
-            kp = self.kdict[1]
-        elif prev<0:
-            kp = self.kdict[0]
-        else:
-            kp = self.kdict[-1]
 
-        if direct>0:
-            self.k = self.kdict[1]
-        elif direct<0:
-            self.k = self.kdict[0]
-        else:
-            self.k = self.kdict[-1]
-        
-        # self.dt = np.absolute(direct)*dt
-        # self.i = int(np.absolute(direct)*10)
-        
+@joy.event
+def on_button(button, pressed):
+    # print('button: %s pressed: %d' % (button, pressed))
+    controller_values[button] = pressed
 
-class joy_controller():
+
+@joy.event
+def on_axis(axis, value):
+    # print('axis: %s value: %f' % (axis, value))
+    controller_values[axis] = value
+
+
+class vjoy_controller():
     """
     class to use pyvjoy to simulate a joystick
     """
+
     def __init__(self, n):
         self.vjoyobj = pyvjoy.VJoyDevice(n)
         self.vjoyobj.data.wAxisX = int(32767/2)
-        self.vjoyobj.data.wAxisY = int(32767/2)   
+        self.vjoyobj.data.wAxisY = int(32767/2)
         self.vjoyobj.data.wAxisZRot = int(32767/2)
         self.vjoyobj.update()
 
-    def iterate(self, dire, speed):
-        self.vjoyobj.data.wAxisX = int(32767*(dire+1)/2)
-        self.vjoyobj.data.wAxisZRot = int(32767/2)+int(32767/2*speed)
+    def iterate(self, steering, throttle):
+        self.vjoyobj.data.wAxisX = int(32767*(steering+1)/2)
+        self.vjoyobj.data.wAxisZRot = int(32767 * throttle)
         self.vjoyobj.update()
-        
-
-if __name__ == "__main__":
-    bbox = (0,33,800,450+33) # (0,33,1024,768+33) # set here the coordinates of your game/screen to capture
-
-    lab_dictkey = ["left", "up", "right"] # keys to be listen when recording
-
-    recording = False
-    avlist = []
-    memory_size = 30
-    prev = 0
-
-    model = load_model('model.h5', compile=False) # set here your path tou your model if you have one, either you can use this one
-    sct = mss.mss()
-
-    raw = interface.screen(0, stackx=False)
-    c = interface.screen(0, stackx=False)
-
-    font = cv2.FONT_HERSHEY_COMPLEX
-
-    pred_txt = interface.text("", font=font, fontsize=0.5, stackx=True)
-    dire_txt = interface.text("", font=font, fontsize=0.5, stackx=False)
-    key_txt = interface.text("", font=font, fontsize=0.5, stackx=False)
-    autonomous_txt = interface.text("", font=font, fontsize=0.5, stackx=False)
-
-    keys = [0]*3
-
-    canvas = interface.ui(screens=[raw, c], texts=[pred_txt, dire_txt, key_txt, autonomous_txt], name="autonomous_driving")
-    # kj = joy_keyboard()
-    vj = joy_controller(1)
-
-    while(1): # control/recording loop
-
-        sct_img = sct.grab(bbox)
-        raw.img = np.array(Image.frombytes('RGB', sct_img.size, sct_img.rgb))
-
-        raw.img = cv2.cvtColor(raw.img, cv2.COLOR_RGB2BGR)/255
-        img = cv2.resize(raw.img, (320,240))
-        img = img[100:, :, :]
-        img = cv2.resize(img, (160,120))
-        
-        dire = model.predict(np.expand_dims(img, axis=0))[0][0]
-        if dire<0:
-            dire = -(np.absolute(dire)**0.5)
-        else:
-            dire = dire**0.5
-
-        pred_txt.string = "predicted: "+str(dire)
-
-        for k in lab_dictkey:
-            keys[lab_dictkey.index(k)] = keyboard.is_pressed(k)
-        key_txt.string = "manual steering: "+str(keys)
-        autonomous_txt.string = "autonomous: "+str(not(any(keys)==True))
-
-        c.img = cv2.line(np.copy(img), (img.shape[1]//2, img.shape[0]), (int(img.shape[1]/2+dire*30), img.shape[0]-50), color=[1, 0, 0], thickness=4)
-
-        if any(keys)==True:
-            vj.iterate(0, 0)
-        else:
-            vj.iterate(dire, 0)
-        
-        # kj.get_key(dire, prev)
-        # kj.iterate()
-        # prev = dire
 
 
-        canvas.update()
-        canvas.show(factor=(1,1))
-        
-        ### 
-        '''
-        if keyboard.is_pressed('i'): # press "i" to switch from rec/control mode
-            if recording == False:
-                recording = True
-            else:
-                recording = False
-            print("recording: ", recording)
-            time.sleep(0.5)
+class autonomous_driving():
+    def __init__(self, model_name, Dataset, region=(0, 33, 800, 450+33)):
+        self.Dataset = Dataset
+        self.model = architectures.load_model(model_name)
+        architectures.apply_predict_decorator(self.model)
 
-        if recording:
-            lab_keys = []
-            for k in lab_dickey:
-                lab_keys.append(keyboard.is_pressed(k))
-            lab = 0
-            wl = []
-            for it, ks in enumerate(lab_keys):
-                lab+= ks*(it*2)
-                if ks == True:
-                    wl.append(ks*it*2)
+        self.region = region
+        self.d = d3dshot.create(capture_output="numpy", frame_buffer_size=5)
+        self.d.display = self.d.displays[0]
+        self.d.capture()
 
-            if np.average(lab_keys) != 0:
-                mean = int(np.average(wl))
-                to_save = lab_dico[mean]
-                cv2.imwrite('C:\\Users\\maxim\\img_trackmania\\'+str(to_save)+'_'+str(time.time())+'.png', img*255)
-        '''
-        
+        self.frame = np.zeros((160, 120, 3), dtype=np.float32)
+
+        # init virtual joy
+        self.vjoy = vjoy_controller(1)
+
+        # init recording folder
+        self.dos_save = os.getcwd()+"\\recorded\\"
+        if not os.path.isdir(self.dos_save):
+            os.mkdir(self.dos_save)
+
+    def get_latest_frame(self):
+        frame = self.d.get_latest_frame()
+        if frame is not None:
+            self.frame = cv2.cvtColor(
+                frame[
+                    self.region[1]:self.region[3],
+                    self.region[0]:self.region[2]
+                ],
+                cv2.COLOR_RGB2BGR)
+
+    def prepare_frame(self, frame):
+        return cv2.resize(frame, (160, 120))
+
+    def predict(self, frame, annotation_list={}, input_components=[]):
+        to_pred = Dataset.make_to_pred_annotations(
+            [frame], [annotation_list], input_components)
+
+        predicted, dt = self.model.predict(to_pred)
+        predicted = predicted[0]
+        print(predicted)
+
+        return predicted['direction'][0], predicted['throttle'][0]
+
+    def predict_and_drive(self):
+        self.get_latest_frame()
+        frame = self.prepare_frame(self.frame)
+        steering, throttle = self.predict(frame)
+        self.vjoy.iterate(steering, throttle)
+
+    def labelize(self):
+        if controller_values[15]:  # X button
+            self.get_latest_frame()
+            to_save = self.prepare_frame(self.frame)
+            self.save_frame(to_save, controller_values)
+
+    def save_frame(self, frame, controller_values):
+        steering = controller_values['l_thumb_x'] * 2
+        throttle = controller_values['right_trigger'] - \
+            controller_values['left_trigger']
+
+        Dataset.save_img_and_annotation(
+            frame,
+            {
+                'direction': float(steering),
+                'throttle': float(throttle),
+                'time': time.time()
+            },
+            self.dos_save)
+
+
+if __name__ == '__main__':
+    Dataset = dataset_json.Dataset(
+        ['direction', 'speed', 'throttle', 'time'])
+
+    autocar = autonomous_driving(
+        "C:\\Users\\maxim\\GITHUB\\AutonomousCar\\test_model\\models\\trackmania.h5",
+        Dataset)
+
+    while(True):
+        joy.dispatch_events()
+
+        # autocar.labelize()
+        autocar.predict_and_drive()
+
+    cv2.destroyAllWindows()
